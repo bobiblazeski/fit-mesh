cc1d717c42c77be903b9853076e27a58f54a0f67
Slobodan Blazeski <slobodan.blazeski@gmail.com>
2022-01-10 12:14:53 +0100

    Fit geoid

--- src/cleansed_cube.py
+++ src/cleansed_cube.py
@@ -152,6 +152,18 @@ def sides_dict(n):
 
 Mesh = namedtuple('Mesh', ['vertices', 'faces', 'colors'])
 
+def cube_to_sphere(bxyz):
+    x, y, z = bxyz[:, 0, :, :], bxyz[:, 1, :, :], bxyz[:, 2, :, :]
+    r = torch.sqrt(x**2 + y**2 + z**2)
+    x_angle = torch.atan2(y, x)
+    z_angle = torch.acos(z/r)
+    
+    r = torch.ones_like(x) * 0.5
+    x = r * torch.sin(z_angle) * torch.cos(x_angle)
+    y = r * torch.sin(z_angle) * torch.sin(x_angle)
+    z = r * torch.cos(z_angle)        
+    return torch.stack((x, y, z), dim=1)   
+
 class SourceCube(nn.Module):
     def __init__(self, n, start=-0.5, end=0.5):
         super(SourceCube, self).__init__()
@@ -160,14 +172,16 @@ class SourceCube(nn.Module):
             torch.linspace(start, end, steps=n))
         d3 = torch.full_like(d1, end) + 1 / n
         sides =  OrderedDict({
-            'front': torch.stack((+d3,  d1,  d2), dim=-1),
-            'right': torch.stack(( d1, +d3,  d2), dim=-1),    
-            'back' : torch.stack((-d3,  d1,  d2), dim=-1),         
-            'left' : torch.stack(( d1, -d3,  d2), dim=-1),
-            'top'  : torch.stack(( d1,  d2, +d3), dim=-1),
-            'down' : torch.stack(( d1,  d2, -d3), dim=-1),
+            'front': torch.stack((+d3,  d1,  d2), dim=0),
+            'right': torch.stack(( d1, +d3,  d2), dim=0),    
+            'back' : torch.stack((-d3,  d1,  d2), dim=0),         
+            'left' : torch.stack(( d1, -d3,  d2), dim=0),
+            'top'  : torch.stack(( d1,  d2, +d3), dim=0),
+            'down' : torch.stack(( d1,  d2, -d3), dim=0),
         })
-        vertices = torch.stack(list(sides.values())).reshape(-1, 3)
+        stacked = torch.stack([p for p in sides.values()])
+        sphered = cube_to_sphere(stacked)
+        vertices = sphered.permute(0, 2, 3, 1).reshape(-1, 3)
         self.register_buffer('vertices', vertices)
         self.register_buffer('faces', make_cube_faces(n).int())
         self.register_buffer('colors', torch.ones_like(vertices) * 0.5)
@@ -280,5 +294,122 @@ class ProgressiveCube(nn.Module):
         mesh, _ = self.forward()
         vertices = mesh.vertices[0].cpu().detach()
         faces = mesh.faces.cpu().detach()        
+        mesh = trimesh.Trimesh(vertices=vertices, faces=faces)
+        mesh.export(f)
+
+class Ellipsoid(nn.Module):
+    def __init__(self, n):
+        super(Ellipsoid, self).__init__()        
+        stacked = self.make_cube_sides(n, stacked=True)         
+        x_angle, z_angle = self.get_xz_angles(stacked)        
+        self.register_buffer('x_angle', x_angle)
+        self.register_buffer('z_angle', z_angle)
+        self.register_buffer('faces', make_cube_faces(n).int())        
+        self.register_buffer('colors', torch.ones(torch.numel(x_angle), 3) * 0.5)
+
+    def to_vertices(_, t):
+        return t.permute(0, 2, 3, 1).reshape(-1, 3)
+    
+    def get_xz_angles(_, bxyz):
+        x, y, z = bxyz[:, 0, :, :], bxyz[:, 1, :, :], bxyz[:, 2, :, :]
+        r = torch.sqrt(x**2 + y**2 + z**2)
+        x_angle = torch.atan2(y, x)
+        z_angle = torch.acos(z/r)
+        return x_angle, z_angle
+   
+    def get_ellipsoidal(_, x_angle, z_angle, rs):
+        x = rs[0] * torch.sin(z_angle) * torch.cos(x_angle)
+        y = rs[1] * torch.sin(z_angle) * torch.sin(x_angle)
+        z = rs[2] * torch.cos(z_angle) 
+        return torch.stack((x, y, z), dim=1)
+    
+    def make_cube_sides(_, n, r=0.5, stacked=False):
+        start, end =  -r, +r
+        d1, d2 = torch.meshgrid(
+            torch.linspace(start, end, steps=n),
+            torch.linspace(start, end, steps=n))
+        d3 = torch.full_like(d1, end) + 1 / n
+        sides = OrderedDict({
+            'front': torch.stack((+d3,  d1,  d2), dim=0),
+            'right': torch.stack(( d1, +d3,  d2), dim=0),    
+            'back' : torch.stack((-d3,  d1,  d2), dim=0),         
+            'left' : torch.stack(( d1, -d3,  d2), dim=0),
+            'top'  : torch.stack(( d1,  d2, +d3), dim=0),
+            'down' : torch.stack(( d1,  d2, -d3), dim=0),
+        })
+        if stacked:
+            return torch.stack([p for p in sides.values()])
+        return sides
+    
+    
+    def forward(self, rs):
+        ellipsoidal = self.get_ellipsoidal(self.x_angle, self.z_angle, rs)        
+        vert = self.to_vertices(ellipsoidal)
+        return Mesh(vert, self.faces, self.colors) 
+
+    def export(self, f, rs):        
+        verts, faces, _ =  self.forward(rs)
+        vertices = verts.cpu().detach()
+        faces = faces.cpu().detach()
+        mesh = trimesh.Trimesh(vertices=vertices, faces=faces)
+        mesh.export(f)
+
+class Geoid(nn.Module):
+    def __init__(self, n):
+        super(Geoid, self).__init__()        
+        stacked = self.make_cube_sides(n, stacked=True)         
+        x_angle, z_angle = self.get_xz_angles(stacked)        
+        self.register_buffer('x_angle', x_angle)
+        self.register_buffer('z_angle', z_angle)
+        self.register_buffer('faces', make_cube_faces(n).int())        
+        self.register_buffer('colors', torch.ones(torch.numel(x_angle), 3) * 0.5)
+        
+        self.radii = torch.nn.Parameter(torch.zeros(3, *x_angle.shape))
+
+    def to_vertices(_, t):
+        return t.permute(0, 2, 3, 1).reshape(-1, 3)
+    
+    def get_xz_angles(_, bxyz):
+        x, y, z = bxyz[:, 0, :, :], bxyz[:, 1, :, :], bxyz[:, 2, :, :]
+        r = torch.sqrt(x**2 + y**2 + z**2)
+        x_angle = torch.atan2(y, x)
+        z_angle = torch.acos(z/r)
+        return x_angle, z_angle
+   
+    def get_ellipsoidal(_, x_angle, z_angle, radii):
+        x = radii[0] * torch.sin(z_angle) * torch.cos(x_angle)
+        y = radii[1] * torch.sin(z_angle) * torch.sin(x_angle)
+        z = radii[2] * torch.cos(z_angle) 
+        return torch.stack((x, y, z), dim=1)
+    
+    def make_cube_sides(_, n, r=0.5, stacked=False):
+        start, end =  -r, +r
+        d1, d2 = torch.meshgrid(
+            torch.linspace(start, end, steps=n),
+            torch.linspace(start, end, steps=n))
+        d3 = torch.full_like(d1, end) + 1 / n
+        sides = OrderedDict({
+            'front': torch.stack((+d3,  d1,  d2), dim=0),
+            'right': torch.stack(( d1, +d3,  d2), dim=0),    
+            'back' : torch.stack((-d3,  d1,  d2), dim=0),         
+            'left' : torch.stack(( d1, -d3,  d2), dim=0),
+            'top'  : torch.stack(( d1,  d2, +d3), dim=0),
+            'down' : torch.stack(( d1,  d2, -d3), dim=0),
+        })
+        if stacked:
+            return torch.stack([p for p in sides.values()])
+        return sides
+    
+    
+    def forward(self, radii=None):
+        radii = torch.sigmoid(radii or self.radii)
+        ellipsoidal = self.get_ellipsoidal(self.x_angle, self.z_angle, radii)        
+        vert = self.to_vertices(ellipsoidal)
+        return Mesh(vert, self.faces, self.colors) 
+
+    def export(self, f, radii=None):        
+        verts, faces, _ =  self.forward(radii)
+        vertices = verts.cpu().detach()
+        faces = faces.cpu().detach()
         mesh = trimesh.Trimesh(vertices=vertices, faces=faces)
         mesh.export(f)
